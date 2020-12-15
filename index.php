8a8078b31cf846caa6535fb16051664a9d34eebb
Adr13nn <btr.adrien@gmail.com>
2020-12-15 08:27:22 +0100

    initialisation + routage avec prob vérifs

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+
+session_start();
+
+$route = (isset($_GET["route"]))? $_GET["route"] : "accueil";
+
+switch($route) {
+
+    case "accueil" : $toTemplate = showHome();
+    break;
+    case "register" : $toTemplate = showRegister();
+    break;
+    case 'login' : $toTemplate = showLogin();
+    break;
+    case "formlivre" : $toTemplate = showFormLivre();
+    break;
+    case "newUser" : insert_user();
+    break;
+    case 'connectionUser' : $toTemplate = connect_user();
+    break;
+    default : $toTemplate = ["template" => "404.html"];
+
+}
+
+function showHome(): array {
+
+    $ressource = fopen("compteur.txt", "r");
+    $compteur = fgets($ressource);
+    fclose($ressource);
+    $compteur++;
+    
+    echo $compteur;
+    
+    $ressource = fopen("compteur.txt", "w");
+    fwrite($ressource, $compteur);
+    fclose($ressource);
+
+    return ["template" => "accueil.html"];
+}
+
+
+function showRegister(): array {
+
+    return ["template" => "register.html"];
+}
+
+function showLogin(): array {
+
+    return ["template" => "accueil.html"];
+}
+
+function showFormLivre(): array {
+
+    require_once "models/Livre.php";
+
+    $livres = Livre::getLivres();
+
+    return ["template" => "formulaire.php", "datas" => $livres];
+}
+
+function insert_user() {
+
+    require_once "models/Utilisateur.php";
+
+    $user = new Utilisateur($_POST["pseudo"], $_POST["email"], $_POST["password"], );
+    var_dump($user);
+
+    $user->addId();
+
+    var_dump($user);
+
+
+    $user->save_user();
+
+    // header("Location:index.php?route=accueil");
+    // exit;
+}
+
+function connect_user() {
+    require_once "models/Utilisateur.php";
+
+    $user = new Utilisateur($_POST["pseudo"], $_POST["password"]);
+    // var_dump($user);
+    $user->verify_user();
+
+
+    // if(is_array($user)) {
+    //     header("Location:index.php?route=monEspace");
+    //     exit;
+    // } else {
+    //     header("Location:index.php?route=accueil");
+    //     exit;
+    // }
+    
+}
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- <nav>
+        <ul>
+            <li><a href="index.php?route=accueil">Accueil</a></li>
+            <li><a href="index.php?route=autrepage">Autre page</a></li>
+            <li><a href="index.php?route=formlivre">Formulaire d'ajout d'un livre</a></li>
+        </ul>
+    </nav> -->
+    
+    <?php require "templates/" . $toTemplate['template']; ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ models/Utilisateur.php
@@ -0,0 +1,93 @@
+<?php
+
+
+class Utilisateur {
+
+    private $pseudo;
+    private $email;
+    private $mdp;
+    private $id_utilisateur;
+    
+
+    function __construct(string $pseudo, string $mdp, int $id_utilisateur = 1) {
+        $this->pseudo = $pseudo;
+        $this->mdp = $mdp; 
+        $this->id_utilisateur = $id_utilisateur;
+    }
+
+    function caracteristiques() {
+        $this->pseudo = $pseudo;
+        $this->email = $email;
+        $this->mdp = $mdp;
+        $this->id_utilisateur = $id_utilisateur++; 
+    }
+
+    function addId(){
+        $this->id_utilisateur++;
+    }
+
+    function save_user() {
+
+        //echo "Je récupère le contenu de mon fichier livres.json :<br>";
+        $contenu = (file_exists("datas/users.json"))? file_get_contents("datas/users.json") : "";
+        //var_dump($contenu);
+
+        //echo "Je décode mon JSON en structure PHP (tableau associatif) :<br>";
+        $users = json_decode($contenu);
+        //var_dump($livres);
+   
+        $users = (is_array($users))? $users : [];
+    
+        $user = get_object_vars($this);
+        array_push($users, $user);
+        $handle = fopen("datas/users.json", "w");
+        $json = json_encode($users);
+        fwrite($handle, $json);
+        fclose($handle);   
+    }
+
+    function verify_user() {
+        $contenu = file_get_contents("datas/users.json");
+        $users = json_decode($contenu);
+        $user = get_object_vars($this);
+
+        // var_dump($user);
+        // var_dump($users);
+
+        if($user["pseudo"] == $users["pseudo"] && $user["mdp"] == $users["mdp"]) {
+            $this-> getUser();
+            
+            $_SESSION["savedUser"] = $user;
+            
+        } else {
+            array_push($users, $user);
+            $handle = fopen("datas/users.json", "w");
+            $json = json_encode($users);
+            fwrite($handle, $json);
+            fclose($handle);
+        }
+    }
+
+    function getUser() {
+        return $this->$pseudo;
+        return $this->$email;
+        return $this->$mdp;
+        return $this->$id_utilisateur;
+    }
+
+
+    static function getUsers(): array {
+
+        //echo "Je récupère le contenu de mon fichier livres.json :<br>";
+        $contenu = (file_exists("datas/users.json"))? file_get_contents("datas/users.json") : "";
+        //var_dump($contenu);
+
+        //echo "Je décode mon JSON en structure PHP (tableau associatif) :<br>";
+        $users = json_decode($contenu);
+        //var_dump($livres);
+
+        $users = (is_array($users))? $users : [];
+
+        return $users;
+    }
+}
